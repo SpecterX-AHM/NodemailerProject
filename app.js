19ff0d1509ffa4acabd3f33c7488883a384905f4
DamianArado <khanhumayun95@gmail.com>
2021-01-27 01:25:51 +0530

    Add files👀

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+//Transporter object
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  });
+
+//mailOptions object
+let mailOptions = {
+    from: 'sender@gmail.com',
+    to: 'receiver@gmail.com',
+    subject: 'NodemailerProject',
+    text: 'Hi! I\'m your nodemailer project'
+  };
+
+//sendMail method
+transporter.sendMail(mailOptions, function(err, data) {
+    if(err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`)
+});
